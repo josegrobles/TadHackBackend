bcd95c11828c91d6ac5a9874b3729178d42eb2d1
Jose Antonio <josegrobles1997@gmail.com>
2017-09-29 15:47:27 +0200

    Fixed print

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/register', function(req, res, next) {
   let body = req.body
-  print(body)
+  console.log(body)
   res.send('respond with a resource');
 });
 
